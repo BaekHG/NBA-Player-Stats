211aeff8cb1622b6f3b5d27b9340990531e1d2c9
brobaik <hhyungi94@gmail.com>
2020-06-09 00:27:02 +0900

    complete search.js

--- src/components/Search.js
+++ src/components/Search.js
@@ -12,138 +12,130 @@ import Paper from '@material-ui/core/Paper';
 import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles({
   textField: {
-    color: 'white',
-    width: '1000px',
+    color: '#E2E5E7',
+    width: '700px',
+    backgroundColor: 'transparent',
+  },
+  paper: {
+    position: 'absolute',
+    width: '700px',
+    maxHeight: 300,
+    overflow: 'scroll',
+    backgroundColor: '#212121',
+    color: '#E2E5E7',
   },
 });
 
 const Search = ({ addToPlayer }) => {
   const classes = useStyles();
   const [text, setText] = useState('');
-  const [state, setState] = useState([]);
-  const onChange = (value) => {
-    // setText(e.target.value);
+  const [state, setState] = useState({
+    playerinfo: [],
+  });
+  const onInputValueChange = (value) => {
     setText(value);
-    executeSearch();
+    console.log(value);
   };
   const executeSearch = debounce(async () => {
     if (isEmpty(text) || text.length < 3) return;
     getPlayer();
   }, 350);
 
-  const myRef = React.useRef(null);
-
+  useEffect(() => {
+    executeSearch();
+    return () => {
+      executeSearch.cancel();
+    };
+  }, [text]);
   const getPlayer = () => {
     axios
       .get(`https://www.balldontlie.io/api/v1/players?search=${text}`)
       .then(async (res) => {
-        // const playerInfo = res.data.data[0];
-        // await getPlayerStats(playerInfo);
-        // console.log(res.data.data);
         setState({ playerinfo: res.data.data });
-        console.log(state);
       })
       .catch((err) => {
         console.log(err);
       });
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    // addToPlayer(state);
-    console.log(state.playerinfo);
-    console.log(typeof state.playerinfo);
-    console.log(state.playerinfo[0].first_name);
-    setText('');
-  };
-
-  const renderSuggestions = () => {
+  const renderSuggestions = ({
+    highlightedIndex,
+    selectedItem,
+    getItemProps,
+  }) => {
     if (state.playerinfo === undefined) return;
-    const render = state.playerinfo.map((player) => (
-      <li>
-        {player.first_name} {player.last_name} - {player.team.abbreviation}
-      </li>
-    ));
-    return render;
-  };
-  const { playerinfo } = state;
-  return (
-    <Container>
-      <Downshift
-        id="downshift-simple"
-        onInputValueChange={onChange}
-        itemToString={(item) => (item ? item.name : '')}
+    return state.playerinfo.map((player, index) => (
+      <MenuItem
+        {...getItemProps({ item: player })}
+        key={player.id}
+        selected={highlightedIndex === index}
+        component="div"
+        style={{
+          fontWeight: 400,
+        }}
       >
-        {({
-          getInputProps,
-          getItemProps,
-          getMenuProps,
-          highlightedIndex,
-          isOpen,
-          selectedItem,
-          inputValue,
-          clearSelection,
-          getRootProps,
-        }) => (
-          //   <form onSubmit={handleSubmit}>
-          <div
-            {...getRootProps(
-              {
-                //   ref: myRef,
-                //   refKey: 'ref',
-              },
-              { suppressRefError: true }
-            )}
-          >
-            <TextField
-              fullWidth
-              varient="outlined"
-              className={classes.textField}
-              type="text"
-              value={text}
-              InputProps={{
-                ...getInputProps(),
-                className: classes.textField,
-              }}
-              placeholder="SEARCH FOR A PLAYER e.g. Lebron James"
-            />
-            {/* <div {...getMenuProps()}>
-              {isOpen && playerinfo.length > 0 ? (
-                <Paper className={classes.paper} square>
-                  {this.renderSuggestions({
-                    highlightedIndex,
-                    selectedItem,
-                    getItemProps,
-                  })}
-                </Paper>
-              ) : null}
-            </div> */}
+        {player.first_name} {player.last_name} - {player.team.abbreviation}
+      </MenuItem>
+    ));
+  };
+
+  const onChange = (item) => {
+    addToPlayer(item);
+  };
+  return (
+    <Downshift
+      id="downshift-simple"
+      onInputValueChange={onInputValueChange}
+      onChange={onChange}
+      itemToString={(item) => (item ? item.name : '')}
+    >
+      {({
+        getInputProps,
+        getItemProps,
+        getMenuProps,
+        highlightedIndex,
+        isOpen,
+        selectedItem,
+        inputValue,
+        clearSelection,
+        getRootProps,
+      }) => (
+        <div
+          className={classes.container}
+          {...getRootProps({}, { suppressRefError: true })}
+        >
+          <TextField
+            fullWidth
+            varient="outlined"
+            className={classes.textField}
+            type="text"
+            value={text}
+            InputProps={{
+              ...getInputProps(),
+              className: classes.textField,
+            }}
+            placeholder="SEARCH FOR A PLAYER e.g. Lebron James"
+          />
+          <div {...getMenuProps()}>
+            {isOpen && state.playerinfo.length > 0 ? (
+              <Paper className={classes.paper} square>
+                {renderSuggestions({
+                  highlightedIndex,
+                  selectedItem,
+                  getItemProps,
+                })}
+              </Paper>
+            ) : null}
           </div>
-          //   </form>
-        )}
-      </Downshift>
-    </Container>
+        </div>
+      )}
+    </Downshift>
   );
 };
 
-const mapDispatchToProps = (dispatch, ownProps) => {
+const mapDispatchToProps = (dispatch) => {
   return {
     addToPlayer: (state) => dispatch(add(state)),
   };
 };
 export default connect(null, mapDispatchToProps)(Search);
-
-const Div = styled.h2`
-  color: white;
-`;
-const Container = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-`;
---
+++ src/components/SeasonAverage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+
+
+const SeasonAverage=  ()=>{
+
+}
+
+export deafult SeasonAverage
