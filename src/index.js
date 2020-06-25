402c250be69c20dcc72cc00eaa47a4edb743e470
brobaik <hhyungi94@gmail.com>
2020-06-25 18:15:06 +0900

    deploy

--- src/index.js
+++ src/index.js
@@ -3,6 +3,23 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import { Provider } from 'react-redux';
 import store from './store';
+
+// const express = require('express');
+// const path = require('path');
+// const app = express()
+// const publicPath = path.join(__dirname,'..','public');
+// const PORT = process.env.PORT || 3000;
+
+// app.use(express.static(publicPath));
+
+// app.get("*", (req,res) => {
+//   res.sendFile(path.join(publicPath,'index.html'))
+// })
+
+// app.listen(PORT,()=>{
+//   console.log('Server is up!')
+// });
+
 ReactDOM.render(
   <Provider store={store}>
     <App />
