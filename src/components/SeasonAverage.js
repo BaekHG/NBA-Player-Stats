aa69e00026a8cb9bfd17f60d97cb8a5376ac8d9c
brobaik <hhyungi94@gmail.com>
2020-07-26 20:20:14 +0900

    test

--- src/components/SeasonAverage.js
+++ src/components/SeasonAverage.js
@@ -7,6 +7,7 @@ import { colors } from './theme';
 import PlayerInfo from './PlayerInfo';
 import Modal from '@material-ui/core/Modal';
 const SeasonAverage = ({ info }) => {
+  console.log('info');
   const [open, setOpen] = useState({
     isOpen: false,
     i: 1,
